3590d5b596210e70179c5c910d301cac2c27ec8e
Tamas K. Papp <tkpapp@gmail.com>
2017-08-15 17:39:49 +0200

    Deleted unused files.
    Content has been moved to a single file.

--- test/test-integral.jl
+++
@@ -1,6 +0,0 @@
-using Cubature
-
-@testset "Univariate integral" begin
-    f, dom = integral_substitution(INVODDSRATIO, x->exp(-x^2), 0..Inf)
-    @test hquadrature(f, dom.left, dom.right)[1] ≈ √π/2
-end
